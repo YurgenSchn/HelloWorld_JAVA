780e0af9c711aa1ba86db65fda0f7a9659e91310
YurgenSchn <yurgen.schn@gmail.com>
2018-08-15 16:13:40 -0300

    Ver 1.1 - 15/08

--- src/br/com/senai/teste/TesteCasting.java
+++ src/br/com/senai/teste/TesteCasting.java
@@ -7,16 +7,23 @@ public class TesteCasting {
 		//Apesar 6 ser um inteiro o valor da variável grande
 		//Continua sendo do tipo long
 		
-		long grande = 6;
-		//int pequeno = 16L;
+		long grande = 6L;  //NUMEROS 'L' SÓ CABEM EM TIPOS LONG
+		int pequeno = 16;
 		
-		float a = 12.121F;  //Correto
-		//float b = 35.125;	//35.125 é um double - incorreto
+		float a = 12.121F;  //Correto 
+		float b = 35.125F;	//35.125 é um double - PRECISA DO F
+			
 		
-		long grandea = 6;
-		int pequenoa= (int)99L;
-		float aa=12.121F;
-		float bb= (float)aa;
+		//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+		//-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-//
+		//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+		
+		
+		int pequenoa= (int)99L; //Para definir um valor 'L' dentro de um tipo
+								//que não suporta, é preciso fazer uma espécie
+								//de conversão, ou casting.
+		double aa = 12.121;
+		float  bb = (float)aa;
 		
 		System.out.println(bb);
 	}
